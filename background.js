34045fd8ba8725ab60a6e13e66b02200ca2535b1
skazancev <stas.kazancev54@gmail.com>
2019-10-13 03:24:27 +0600

    added enabled/disabled icon

--- background.js
+++ background.js
@@ -1,32 +1,45 @@
+var config = {
+  storageKey: 'psycheaExtension',
+  enabledIcon: 'icons/enabled.png',
+  disabledIcon: 'icons/disabled.png',
+  vkClientId: '6885501'
+}
+chrome.config = config;
 chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.get([config.storageKey], function (result) {
+    chrome.browserAction.setIcon({
+      path: result[config.storageKey] === true ? config.enabledIcon : config.disabledIcon
+    });
+  });
+
     window.disable = function() {
     chrome.storage.sync.set({
-      psycheaExtension: false
-    })
+      [config.storageKey]: false
+    });
+    chrome.browserAction.setIcon({path: config.disabledIcon});
   }
 
   window.enable = function() {
     chrome.storage.sync.set({
-      psycheaExtension: true
+      [config.storageKey]: true
     })
+    chrome.browserAction.setIcon({path: config.enabledIcon});
   };
 
   window.OAuth2 = {
     init: function () {
       this._authorization_url = 'https://oauth.vk.com/authorize'
-      this._client_id = '6885501';
       this._redirect_uri = 'https://oauth.vk.com/blank.html';
-      this._scopes = ['friends'];
       this._version = '5.102';
+      this._client_id = config.vkClientId;
     },
 
     begin: function () {
       var url = this._authorization_url +
         '?client_id=' + this._client_id +
         '&redirect_uri=' + this._redirect_uri +
-        '&scopes=' + this._scopes.join(',') +
         '&v=' + this._version +
-        '&response_type=token&revoke=1';
+        '&response_type=token&scope=offline';
       var self = this;
 
       chrome.tabs.create({url: url, selected: true}, function (data) {
