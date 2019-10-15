c5b50000fca470f8ec70f21879b7f7f6ceb634aa
IK <homeronkis@gmail.com>
2019-10-15 13:00:36 +0600

    append elements

--- content.js
+++ content.js
@@ -8,9 +8,27 @@ function setUserData(userId) {
   xhr.setRequestHeader("Authorization", "Basic " + btoa("psyextention:shikari1"));
   xhr.onload = function(result) {
       var response = JSON.parse(xhr.response);
-      var block =  document.getElementById('profile_short')
-      block.innerHTML = '<p>' + response.first_name + ' ' + response.id + '</p>'
-      console.log(response)
+    function calculation(value, name, spare) {
+      var node = document.createElement("div");
+      if (value <= 50) {
+        return value = 100 - value;
+      } else {
+        name = spare;
+      }
+
+      node.innerHTML = '<span class="head">' + name + ':</span> ' + Math.round(value) + '%';
+
+      return node;
+    }
+    var container = document.getElementById('profile_short')
+    console.log(response.IE);
+    console.log(response.JP);
+    console.log(response.SN);
+    console.log(response.TF);
+      container.appendChild(calculation(response.IE, 'extra', 'intro'))
+      container.appendChild(calculation(response.JP, 'irra', 'racio'))
+      container.appendChild(calculation(response.SN, 'senso', 'intuit'))
+      container.appendChild(calculation(response.TF, 'etic', 'logic'))
   };
   xhr.send();
 }
