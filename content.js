33d5f85f724e8837a0532a915616fb19d8f143fd
skazancev <stas.kazancev54@gmail.com>
2019-10-16 01:26:46 +0600

    fixed getting groups from vk

--- content.js
+++ content.js
@@ -42,22 +42,26 @@ function setUserData(userId) {
       } else {
         name = spare;
       }
-
+      console.log(value)
       node.innerHTML = '<span class="head" style="color: #828282;">' + name + ':</span> ' + Math.round(value) + '%';
       return node;
     }
-    function field(name, value) {
+    function getGroups(name, value) {
       var xhr = new XMLHttpRequest();
       xhr.open(
         'GET',
         'https://api.vk.com/method/groups.getById?group_ids=' + response.groups + '&v=5.102&access_token=44bfe9d644bfe9d644bfe9d63544d6f9ab444bf44bfe9d618c310447a41f43260483d73&fields=name',
-        true
+        false
       );
-      console.log(response.screen_name);
+      xhr.send()
+      groups = JSON.parse(xhr.response).response;
+      groups = groups.map(function(el) {
+          return '<a href="/id' + el.id + '">' + el.name + '</a>';
+      })
       var node = document.createElement("div");
       node.className += 'extension_field_';
 
-      node.innerHTML = ''+ name + ': <p  class="head link_mbti" style="color: #2a5885; width: 200px;">' +  value + ':<p/> ' ;
+      node.innerHTML = ''+ name + ': <p  class="head link_mbti" style="color: #2a5885; width: 200px;">' +  groups.join(', ') + ':<p/> ' ;
 
       return node;
     }
@@ -90,7 +94,7 @@ function setUserData(userId) {
   `;
     document.head.appendChild(style);
     var container = document.getElementById('profile_short');
-    container.prepend(field('Общие группы', response.groups));
+    container.prepend(getGroups('Общие группы', response.groups));
     container.prepend(calculation(response.IE, 'Экстраверт', 'Интроверт'));
       container.prepend(calculation(response.JP, 'Иррационал', 'Рационал'));
       container.prepend(calculation(response.SN, 'Интуит', 'Сенсорик'));
